44f9ad038a9d27c329bdb19b5c44e8fcc450ac22
ahoiberg <andrew@andrewhoiberg.com>
2018-03-06 15:28:45 -0500

    allow data to actually be submitted in input

--- src/InputContainer.js
+++ src/InputContainer.js
@@ -4,39 +4,46 @@ import ReactDOM from 'react-dom'
 export default class InputContainer extends React.Component {
     constructor(){
         super();
-        this.location = {address: '', city: '', state: '', zip: ''}
-
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
+        this.state = {address: '', city: '', st: '', zip: ''};
+      this.handleAddressChange = this.handleAddressChange.bind(this)
+      this.handleCityChange = this.handleCityChange.bind(this)
+      this.handleStChange = this.handleStChange.bind(this)
+      this.handleZipChange = this.handleZipChange.bind(this)
     }
 
-    handleChange(event){
-        this.setState = {[event.target.name]: event.target.value}
+    handleAddressChange(event) {
+      this.setState({address: event.target.value})
+    }
+    handleCityChange(event){
+      this.setState({city: event.target.value})
+    }
+    handleStChange(event){
+      this.setState({st: event.target.value})
     }
 
-    handleSubmit(event) {
-        event.preventDefault();
+    handleZipChange(event){
+      this.setState({zip: event.target.value})
     }
-//todo can't actually type in input...
+
     render() {
     return (
-      <form onSubmit={this.handleSubmit}>
+      <form>
       <h2> Where do you live?: </h2>
         <label>
           address:
-          <input type="text" name="address" value={this.location.address} onChange={this.handleChange} />
+          <input type="text" name="address" onChange={this.handleAddressChange} />
         </label>
         <label>
           city:
-          <input type="text" name="city" value={this.location.city} onChange={this.handleChange} />
+          <input type="text" name="city" onChange={this.handleCityChange} />
         </label>
         <label>
           state:
-          <input type="text" name="state" value={this.location.state} onChange={this.handleChange} />
+          <input type="text" name="state" onChange={this.handleStChange} />
         </label>
         <label>
           zip:
-          <input type="text" name="zip" value={this.location.zip} onChange={this.handleChange} />
+          <input type="text" name="zip" onChange={this.handleZipChange} />
         </label>
         <input type="submit" value="Submit" />
       </form>
